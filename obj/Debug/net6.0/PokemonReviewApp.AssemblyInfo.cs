b0634f2b917a6ec4d905cd7500598ba1eea6d5cc
buyucuk <90260540+akaraol@users.noreply.github.com>
2023-11-23 20:11:49 +0300

    Interfaces and Repository added

--- obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a259fb21fed404880b25d7abba3e1feb45d8936a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ed8ab71286b05e9a26347a95fa91a2fe26a3fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
