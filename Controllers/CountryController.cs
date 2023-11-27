4bcd26837246a5a19bf369ca7246af7a13b81b86
buyucuk <90260540+akaraol@users.noreply.github.com>
2023-11-27 23:42:18 +0300

    Get methods for country, pokemon and category finished and in working condition.

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
 
 namespace PokemonReviewApp.Controllers
 {
@@ -6,6 +11,59 @@ namespace PokemonReviewApp.Controllers
     [ApiController]
     public class CountryController : Controller
     {
-        
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public CountryController(ICountryRepository countryRepository, IMapper mapper)
+        {
+            _countryRepository = countryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+        public IActionResult GetCountries()
+        {
+            var countries = _mapper.Map<List<CountryDto>>(_countryRepository.GetCountries());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(countries);
+        }
+        [HttpGet("{countryId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountry(int countryId)
+        {
+            if (!_countryRepository.CountryExists(countryId))
+            {
+                return NotFound();
+            }
+            var country = _mapper.Map<CountryDto>(_countryRepository.GetCountry(countryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(country);
+        }
+
+        [HttpGet("/owners/{ownerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        public IActionResult GetCountryOfAnOwner(int ownerId)
+        {
+            var country = _mapper.Map<CountryDto>(
+                _countryRepository.GetCountryByOwner(ownerId));
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(country);
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 //AddTransient is going to add the injection at the very beginning
 builder.Services.AddTransient<Seed>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
